1a965332c68ba274d0e68ac220e627f3cda917da
Adrian Price-Whelan <adrian.prw@gmail.com>
2021-08-31 08:03:12 -0400

    formatting

--- src/cpp_gala/lib/src/cpp_gala/simulation/simulation.cpp
+++ src/cpp_gala/lib/src/cpp_gala/simulation/simulation.cpp
@@ -35,33 +35,27 @@ void Simulation::get_acceleration(Body *body, double t, double *acc) {
     Compute the acceleration on a body given all other elements (bodies, external potentials, and
     forces) in the simulation.
     */
-    int i, j;
+    int i;
 
     // Zero out any existing acceleration values
-    for (i=0; i < (body->nbodies * body->ndim); i++) {
+    for (i=0; i < (body->nbodies * body->ndim); i++)
         acc[i] = 0.;
-    }
 
     if (this->potential != NULL) {
-        // Loop over each object in the input body:
-        for (int i=0; i < body->nbodies; i++) {
-            // Compute the acceleration from the simulation external potential, if set
+        // Compute the acceleration from the simulation external potential, if set
+        for (i=0; i < body->nbodies; i++)
             this->potential->acceleration(&body->w[2 * body->ndim * i],
                                           t,
                                           &acc[body->ndim * i]);
-
-            // Acceleration from forces registered to this body:
-            // TODO:
-
-        }
     }
 
     // Compute the acceleration from all other bodies
-    for (const auto &pair : this->bodies) {
+    for (const auto &pair : this->bodies)
         pair.second->get_acceleration(body, t, acc);
-    }
 
-    //return
+
+    // TODO: Compute acceleration from additional forces registered to this body
+
 }
 
 
