067379a7f4ef0c5dbf8100492d8f32637ebc1be7
Adrian Price-Whelan <adrian.prw@gmail.com>
2021-09-15 13:47:29 -0400

    collect cpp std defs

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@ from glob import glob
 from setuptools import setup
 from pybind11.setup_helpers import Pybind11Extension
 
+CPP_STD = 14  # C++14
+
 # sorted(glob("src/cpp_gala/potential/*.cpp")),
 ext_modules = [
     Pybind11Extension(
@@ -10,7 +12,7 @@ ext_modules = [
          "src/cpp_gala/lib/src/cpp_gala/potential/potential.cpp",
          "src/cpp_gala/lib/src/cpp_gala/potential/potentialparameter.cpp",
          "src/cpp_gala/lib/src/cpp_gala/potential/builtin.cpp",],
-        cxx_std=14,
+        cxx_std=CPP_STD,
         include_dirs=['src/cpp_gala/lib/include'],
         libraries=['boost_system']
     ),
@@ -20,7 +22,7 @@ ext_modules = [
          "src/cpp_gala/lib/src/cpp_gala/simulation/simulation.cpp",
          "src/cpp_gala/lib/src/cpp_gala/simulation/particle.cpp",
          "src/cpp_gala/lib/src/cpp_gala/simulation/frame.cpp"],
-        cxx_std=14,
+        cxx_std=CPP_STD,
         include_dirs=['src/cpp_gala/lib/include'],
         # libraries=['gsl']
     ),
@@ -28,12 +30,10 @@ ext_modules = [
         "cpp_gala._integrate",
         ["src/cpp_gala/integrate/interface.cpp",
          "src/cpp_gala/lib/src/cpp_gala/integrate/integrate.cpp"],
-        cxx_std=14,
+        cxx_std=CPP_STD,
         include_dirs=['src/cpp_gala/lib/include'],
         libraries=['boost_system']
     ),
 ]
 
-setup(
-    ext_modules=ext_modules
-)
+setup(ext_modules=ext_modules)
